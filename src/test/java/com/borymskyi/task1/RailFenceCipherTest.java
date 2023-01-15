5fd26cb1a3d7492b9bd622b1d2e33ba8a080b8f5
dborymskyi <dimokskyn@gmail.com>
2023-01-15 13:52:36 +0200

    added more unit tests for 1 task

--- src/test/java/com/borymskyi/task1/RailFenceCipherTest.java
+++ src/test/java/com/borymskyi/task1/RailFenceCipherTest.java
@@ -2,18 +2,43 @@ package com.borymskyi.task1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RailFenceCipherTest {
 
     private final RailFenceCipher railFenceCipher = new RailFenceCipher();
-    private final String expectedDecryptedStr = "WEAREDISCOVEREDFLEEATONCE";
-    private final String expectedEncodedStr = "WECRLTEERDSOEEFEAOCAIVDEN";
 
     @Test
     public void testEncodingString() {
-        var encodedStr = this.railFenceCipher.encodingString(3, this.expectedDecryptedStr);
-        assertEquals(this.expectedEncodedStr, encodedStr);
+        var encoded1 = this.railFenceCipher.encodingString(3, getExpectedDecryptedStrings().get(0));
+        var encoded2 = this.railFenceCipher.encodingString(4, getExpectedDecryptedStrings().get(1));
+        var encoded3 = this.railFenceCipher.encodingString(7, getExpectedDecryptedStrings().get(0));
+        var encoded4 = this.railFenceCipher.encodingString(6, getExpectedDecryptedStrings().get(1));
+        var encoded5 = this.railFenceCipher.encodingString(50, getExpectedDecryptedStrings().get(2));
+        assertAll(() -> {
+            assertEquals("WECRLTEERDSOEEFEAOCAIVDEN", encoded1);
+            assertEquals("bkosyrmiy", encoded2);
+            assertEquals("WREEEECAVDNROFOECLTDSEAIE", encoded3);
+            assertEquals("boriyymks", encoded4);
+            assertEquals("vectorSoftware", encoded5);
+        });
+
+    }
+
+    @Test
+    public void testDecodingString() {
+        var decrypted1 = this.railFenceCipher.decodingString(3, "WECRLTEERDSOEEFEAOCAIVDEN");
+        var decrypted2 = this.railFenceCipher.decodingString(4, "bkosyrmiy");
+        var decrypted3 = this.railFenceCipher.decodingString(7, "WREEEECAVDNROFOECLTDSEAIE");
+        var decrypted4 = this.railFenceCipher.decodingString(50, "vectorSoftware");
+
+        assertEquals(getExpectedDecryptedStrings().get(0), decrypted1);
+        assertEquals(getExpectedDecryptedStrings().get(1), decrypted2);
+        assertEquals(getExpectedDecryptedStrings().get(0), decrypted3);
+        assertEquals(getExpectedDecryptedStrings().get(2), decrypted4);
     }
 
     @Test
@@ -22,10 +47,12 @@ class RailFenceCipherTest {
         assertEquals("", encodedStr);
     }
 
-    @Test
-    public void testDecodingString() {
-        var decryptedStr = this.railFenceCipher.decodingString(3, this.expectedEncodedStr);
-        assertEquals(this.expectedDecryptedStr, decryptedStr);
+    public List<String> getExpectedDecryptedStrings(){
+        return List.of(
+                "WEAREDISCOVEREDFLEEATONCE",
+                "borymskyi",
+                "vectorSoftware"
+        );
     }
 
 }
